d1c48fc9f44303ab879b82e8b52a885e87729a98
Everton Ribeiro <nuxlli@gmail.com>
2015-03-03 00:16:32 -0300

    Fixing context, this is not a global state in function scope.

--- index.js
+++ index.js
@@ -1,94 +1,88 @@
 var crypto = require('crypto');
 
-/**
- *
- * @param {String} secret
- */
-module.exports = function(secret) {
-
+function RailsSessionDecode(secret) {
   this.secret = secret;
   this.cookieSalt = 'encrypted cookie'; //Rails.application.config.action_dispatch.encrypted_cookie_salt
   this.signedCookieSalt = 'signed encrypted cookie'; //Rails.application.config.action_dispatch.encrypted_signed_cookie_salt
   this.iterations = 1000;
   this.keyLength = 64;
+}
 
-  function decodeCookie(cookie, isSignedCookie, next) {
-    if (!cookie) {
-      return next(new Error('cookie was empty.'))
-    }
+function decodeCookie(cookie, isSignedCookie, next) {
+  if (!cookie) {
+    return next(new Error('cookie was empty.'))
+  }
 
-    var cookieSegments = cookie.split('--');
-    if (cookieSegments.length != 2) {
-      return next(new Error('invalid cookie format.'));
-    }
+  var cookieSegments = cookie.split('--');
+  if (cookieSegments.length != 2) {
+    return next(new Error('invalid cookie format.'));
+  }
 
-    var sessionData = new Buffer(cookieSegments[0], 'base64');
+  var sessionData = new Buffer(cookieSegments[0], 'base64');
 //    var signature = cookieSegments[1];
 
-    var sessionDataSegments = sessionData.toString('utf8').split('--');
-    if (sessionDataSegments.length != 2) {
-      return next(new Error('invalid cookie format.'));
+  var sessionDataSegments = sessionData.toString('utf8').split('--');
+  if (sessionDataSegments.length != 2) {
+    return next(new Error('invalid cookie format.'));
+  }
+
+  var data = new Buffer(sessionDataSegments[0], 'base64');
+  var iv = new Buffer(sessionDataSegments[1], 'base64');
+  var salt = isSignedCookie ? this.signedCookieSalt : this.cookieSalt;
+
+  crypto.pbkdf2(this.secret, salt, this.iterations, this.keyLength, function(err, derivedKey) {
+    if (err) return next(err);
+
+    try {
+      var decipher = crypto.createDecipheriv('aes-256-cbc', derivedKey.slice(0, 32), iv.slice(0, 16));
+      var decryptedData = decipher.update(data, 'binary', 'utf8') + decipher.final('utf8');
+
+      next(null, decryptedData);
+    } catch(e) {
+      next(e);
     }
+  });
+}
 
-    var data = new Buffer(sessionDataSegments[0], 'base64');
-    var iv = new Buffer(sessionDataSegments[1], 'base64');
-    var salt = isSignedCookie ? this.signedCookieSalt : this.cookieSalt;
+RailsSessionDecode.prototype = {
+  decodeCookie: function(cookie, next) {
+    return decodeCookie.call(this, cookie, false, next);
+  },
 
-    crypto.pbkdf2(this.secret, salt, this.iterations, this.keyLength, function(err, derivedKey) {
-      if (err) return next(err);
+  decodeSignedCookie: function(cookie, next) {
+    return decodeCookie.call(this, cookie, true, next);
+  },
 
-      try {
-        var decipher = crypto.createDecipheriv('aes-256-cbc', derivedKey.slice(0, 32), iv.slice(0, 16));
-        var decryptedData = decipher.update(data, 'binary', 'utf8') + decipher.final('utf8');
-
-        next(null, decryptedData);
-      } catch(e) {
-        next(e);
-      }
-    });
-  };
-
-
-  this.decodeCookie = function(cookie, next) {
-    return decodeCookie(cookie, false, next);
-  };
-
-  this.decodeSignedCookie = function(cookie, next) {
-    return decodeCookie(cookie, true, next);
-  };
-
-  this.setSecret = function(newSecret) {
+  setSecret: function(newSecret) {
     this.secret = newSecret;
-  };
+  },
 
-  this.setCookieSalt = function(newCookieSalt) {
+  setCookieSalt: function(newCookieSalt) {
     this.cookieSalt = newCookieSalt;
-  };
+  },
 
-  this.setSignedCookieSalt = function(newSignedCookieSalt) {
+  setSignedCookieSalt: function(newSignedCookieSalt) {
     this.signedCookieSalt = newSignedCookieSalt;
-  };
+  },
 
-  this.setIterations = function(newIterations) {
+  setIterations: function(newIterations) {
     this.iterations = newIterations;
-  };
+  },
 
-  this.setKeyLength = function(newKeyLength) {
+  setKeyLength: function(newKeyLength) {
     this.keyLength = newKeyLength;
-  };
+  },
+}
 
-  return {
-    secret: this.secret,
-    cookieSalt: this.cookieSalt,
-    signedCookieSalt: this.signedCookieSalt,
-    iterations: this.iterations,
-    keyLength: this.keyLength,
-    decodeCookie: this.decodeCookie,
-    decodeSignedCookie: this.decodeSignedCookie,
-    setSecret: this.setSecret,
-    setCookieSalt: this.setCookieSalt,
-    setSignedCookieSalt: this.setSignedCookieSalt,
-    setIterations: this.setIterations,
-    setKeyLength: this.setKeyLength
-  };
+/**
+ *
+ * @param {String} secret
+ */
+module.exports = function(secret) {
+  return new RailsSessionDecode(secret);
 };
+
+/**
+ * Rails Session Decoder
+ */
+module.exports.RailsSessionDecode = RailsSessionDecode;
--- index.test.js
+++ index.test.js
@@ -6,132 +6,143 @@ var secret = '52541783ebfc236dc27e1d83cba2a4144b484897995bdf4d9a9977623987ee10b6
 var cookie = 'N0paYjIyWTNIOWgxV2VON0RCM1AvenZzQVNFeWY0elBoQkZ5SnN4OVAybXZQMEErV0VGa1luM2VmYTg4cEk0Y2paVUtMUW8xbEQyUE5VbFJ1OTZUeWJiODdYNkxZSWxvYUtiaE1ucy9LM1BMUy8yd0N0ZExZQzYzUVFsaGZ4M044MjdOdWNJYWhMbW5HOTJpY2UzQUdBPT0tLWtuWk9IWVJpakpWak5oSmZ2d2VLbWc9PQ==--d4292397f777c8f79655884b3fcc241e4bc2fcf5';
 var session = JSON.parse('{"session_id":"1cc5440b929e539280d94888629565d1","_csrf_token":"CzzmfmhiXOMfGDsL4wkUNsvgyjG7215I73e6bXX1MlQ="}');
 
-var decoder = sessionDecoder(secret);
+describe('sessionDecoder', function() {
+  var decoder = null;
 
-describe('Constructor', function() {
-  it('stores the secret', function() {
-    decoder.secret.should.be.exactly(secret);
-  })
-});
-
-describe('Defaults', function(){
-
-  it('has the correct cookieSalt', function() {
-    decoder.cookieSalt.should.be.exactly('encrypted cookie');
+  beforeEach(function() {
+    decoder = new sessionDecoder.RailsSessionDecode(secret);
   });
 
-  it('has the correct signedCookieSalt', function() {
-    decoder.signedCookieSalt.should.be.exactly('signed encrypted cookie');
+  describe('Constructor', function() {
+    it('stores the secret', function() {
+      decoder.secret.should.be.exactly(secret);
+    });
+
+    it('support old api', function() {
+      var dec = sessionDecoder();
+      dec.should.be.instanceof(sessionDecoder.RailsSessionDecode);
+    });
   });
 
-  it('has the correct iterations', function() {
-    decoder.iterations.should.be.exactly(1000);
+  describe('Defaults', function(){
+
+    it('has the correct cookieSalt', function() {
+      decoder.cookieSalt.should.be.exactly('encrypted cookie');
+    });
+
+    it('has the correct signedCookieSalt', function() {
+      decoder.signedCookieSalt.should.be.exactly('signed encrypted cookie');
+    });
+
+    it('has the correct iterations', function() {
+      decoder.iterations.should.be.exactly(1000);
+    });
+
+    it('has the correct keyLength', function() {
+      decoder.keyLength.should.be.exactly(64);
+    });
   });
 
-  it('has the correct keyLength', function() {
-    decoder.keyLength.should.be.exactly(64);
+  describe('#decodeCookie', function() {
+    it('returns error when the cookie is not provided', function (done) {
+      decoder.decodeCookie(null, function(err, result) {
+        err.should.be.ok;
+
+        done();
+      })
+    });
+
+    it('returns error when the format invalid', function (done) {
+      decoder.decodeCookie('InvalidCookie', function(err, result) {
+        err.should.be.ok;
+
+        done();
+      })
+    });
+
+    it('returns the expected values', function (done) {
+      decoder.decodeCookie(cookie, function(err, result) {
+        (err == null).should.be.true;
+
+        JSON.parse(result).should.eql(session);
+
+        done();
+      })
+    });
   });
-});
 
-describe('#decodeCookie', function() {
-  it('returns error when the cookie is not provided', function (done) {
-    decoder.decodeCookie(null, function(err, result) {
-      err.should.be.ok;
+  describe('#decodeSignedCookie', function() {
+    it('returns error when the cookie is not provided', function (done) {
+      decoder.decodeSignedCookie(null, function(err, result) {
+        err.should.be.ok;
 
-      done();
+        done();
+      })
+    });
+
+    it('returns error when the format invalid', function (done) {
+      decoder.decodeSignedCookie('InvalidCookie', function(err, result) {
+        err.should.be.ok;
+
+        done();
+      })
+    });
+
+    //TODO: add a test case for signed cookie final value.
+  });
+
+  describe('#setSecret', function() {
+    it('updates the secret', function() {
+      var newSecret = 'new secret';
+      decoder.secret.should.eql(secret);
+
+      decoder.setSecret(newSecret);
+
+      decoder.secret.should.eql(newSecret);
     })
   });
 
-  it('returns error when the format invalid', function (done) {
-    decoder.decodeCookie('InvalidCookie', function(err, result) {
-      err.should.be.ok;
+  describe('#setCookieSalt', function() {
+    it('updates the cookieSalt', function() {
+      var newCookieSalt = 'new cookieSalt';
+      decoder.cookieSalt.should.eql('encrypted cookie');
 
-      done();
+      decoder.setCookieSalt(newCookieSalt);
+
+      decoder.cookieSalt.should.eql(newCookieSalt);
     })
   });
 
-  it('returns the expected values', function (done) {
-    decoder.decodeCookie(cookie, function(err, result) {
-      (err == null).should.be.true;
+  describe('#setSignedCookieSalt', function() {
+    it('updates the signedCookieSalt', function() {
+      var newSignedCookieSalt = 'new signedCookieSalt';
+      decoder.signedCookieSalt.should.eql('signed encrypted cookie');
 
-      JSON.parse(result).should.eql(session);
+      decoder.setSignedCookieSalt(newSignedCookieSalt);
 
-      done();
-    })
-  });
-});
-
-describe('#decodeSignedCookie', function() {
-  it('returns error when the cookie is not provided', function (done) {
-    decoder.decodeSignedCookie(null, function(err, result) {
-      err.should.be.ok;
-
-      done();
+      decoder.signedCookieSalt.should.eql(newSignedCookieSalt);
     })
   });
 
-  it('returns error when the format invalid', function (done) {
-    decoder.decodeSignedCookie('InvalidCookie', function(err, result) {
-      err.should.be.ok;
+  describe('#setIterations', function() {
+    it('updates the iterations', function() {
+      var newIterations = 2000;
+      decoder.iterations.should.eql(1000);
 
-      done();
+      decoder.setIterations(newIterations);
+
+      decoder.iterations.should.eql(newIterations);
     })
   });
 
-  //TODO: add a test case for signed cookie final value.
-});
+  describe('#setKeyLength', function() {
+    it('updates the keyLength', function() {
+      var newKeyLength = 128;
+      decoder.keyLength.should.eql(64);
 
-describe('#setSecret', function() {
-  it('updates the secret', function() {
-    var newSecret = 'new secret';
-    decoder.secret.should.eql(secret);
+      decoder.setKeyLength(newKeyLength);
 
-    decoder.setSecret(newSecret);
-
-    decoder.secret.should.eql(newSecret);
-  })
-});
-
-describe('#setCookieSalt', function() {
-  it('updates the cookieSalt', function() {
-    var newCookieSalt = 'new cookieSalt';
-    decoder.cookieSalt.should.eql(cookieSalt);
-
-    decoder.setCookieSalt(newCookieSalt);
-
-    decoder.cookieSalt.should.eql(newCookieSalt);
-  })
-});
-
-describe('#setSignedCookieSalt', function() {
-  it('updates the signedCookieSalt', function() {
-    var newSignedCookieSalt = 'new signedCookieSalt';
-    decoder.signedCookieSalt.should.eql(signedCookieSalt);
-
-    decoder.setSignedCookieSalt(newSignedCookieSalt);
-
-    decoder.signedCookieSalt.should.eql(newSignedCookieSalt);
-  })
-});
-
-describe('#setIterations', function() {
-  it('updates the iterations', function() {
-    var newIterations = 2000;
-    decoder.iterations.should.eql(1000);
-
-    decoder.setIterations(newIterations);
-
-    decoder.iterations.should.eql(newIterations);
-  })
-});
-
-describe('#setKeyLength', function() {
-  it('updates the keyLength', function() {
-    var newKeyLength = 128;
-    decoder.keyLength.should.eql(64);
-
-    decoder.setKeyLength(newKeyLength);
-
-    decoder.keyLength.should.eql(newKeyLength);
-  })
+      decoder.keyLength.should.eql(newKeyLength);
+    })
+  });
 });
